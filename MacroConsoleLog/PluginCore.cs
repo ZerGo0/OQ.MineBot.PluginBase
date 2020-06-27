0603ea68e0fda53997fac8f61a8ee08f9a414d8b
ZerGo0 <18653821+ZerGo0@users.noreply.github.com>
2020-06-27 11:52:18 +0200

    [ZerGo0 Main] 0 Additions | 2 Modifications | 0 Deletions Modified: MacroConsoleLog/MacroConsoleLog.csproj Modified: MacroConsoleLog/PluginCore.cs

--- MacroConsoleLog/PluginCore.cs
+++ MacroConsoleLog/PluginCore.cs
@@ -4,9 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using OQ.MineBot.PluginBase.Base;
+using OQ.MineBot.PluginBase.Base.Plugin;
+using OQ.MineBot.PluginBase.Bot;
+
 namespace MacroConsoleLog
 {
     public class PluginCore : IStartPlugin
     {
+        /// <summary>
+        /// Should be used to check compatability with the
+        /// current version of the bot.
+        /// </summary>
+        public override void OnLoad(int version, int subversion, int buildversion) { }
+        public override PluginResponse OnEnable(IBotSettings botSettings)
+        {
+            /* Regular plugin content */
+            return base.OnEnable(botSettings);
+        }
     }
 }
